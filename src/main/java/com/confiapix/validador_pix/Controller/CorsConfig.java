fcf1a205e0d70d2c2dae1418d6d4c3593756ca7c
caiotsx <caiotorressouza2@gmail.com>
2025-11-16 22:29:32 -0300

    adicionando o corsconfig

--- src/main/java/com/confiapix/validador_pix/Controller/CorsConfig.java
+++ src/main/java/com/confiapix/validador_pix/Controller/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "https://produtos-frontend-o38v.onrender.com",
+                    "https://confiapix-frontend.onrender.com",
                     "http://localhost:8080",
                     "http://localhost:3000"
                 )
